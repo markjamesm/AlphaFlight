52998617ccd3e2793365269c33235fdd7a271b1b
Mark-James M <contact@markjames.dev>
2025-10-10 18:49:08 +0000

    More Tailwind styling

--- src/flight-data.js
+++ src/flight-data.js
@@ -7,7 +7,6 @@ export async function fetchPlanesInRadius(radius) {
     }
 
     const result = await response.json();
-    // logPlanesInRadius(result);
     displayFlightTable("plane-data-table-body", result);
 
   } catch (error) {
@@ -15,25 +14,28 @@ export async function fetchPlanesInRadius(radius) {
   }
 }
 
-function logPlanesInRadius(planeList) {
-  planeList.ac.forEach(plane => {
-    console.log(`Plane: ${plane.desc}`);
-  });
-}
-
 function displayFlightTable(element, planeList) {
   const table = document.getElementById(element);
   clearTableData(table);
   planeList.ac.forEach(plane => {
     let row = table.insertRow();
+    row.className = "bg-white border-b dark:bg-gray-800 dark:border-gray-700 border-gray-200";
+    
     let callsign = row.insertCell(0);
     callsign.innerHTML = plane.flight;
+    callsign.className = "dark:text-white px-6 py-2";
+
     let type = row.insertCell(1);
     type.innerHTML = plane.desc;
+    type.className = "dark:text-white px-6 py-2"
+
     let altitude = row.insertCell(2);
     altitude.innerHTML = plane.alt_geom.toLocaleString();
+    altitude.className = "dark:text-white px-6 py-2"
+
     let groundSpeed = row.insertCell(3);
     groundSpeed.innerHTML = `${convertKtsToKmh(plane.gs)} km/h`;
+    groundSpeed.className = "dark:text-white px-6 py-2";
   });
 }
 
@@ -42,5 +44,7 @@ export function clearTableData(element) {
 }
 
 function convertKtsToKmh(speedInKts) {
-  return (Number(speedInKts) * 1.852).toFixed(0);
+  const speedInKmh = (Number(speedInKts) * 1.852).toFixed(0);
+  let output = isNaN(speedInKmh) ? "Unknown" : speedInKmh;
+  return output; 
 }
